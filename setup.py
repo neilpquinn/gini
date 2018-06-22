f26fb6bcd093acbb4d96ee2b4ac03e936f2f2225
Neil P. Quinn <mail@neilpquinn.com>
2018-06-22 16:24:35 -0700

    Update setup.py with author and license info

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="oggini",
     version="0.1",
+    author="Olivia Guest",
+    url="https://github.com/oliviaguest/gini",
+    license="Creative Commons CC0"
     description="A function for calculating the Gini coefficient of a NumPy array",
     install_requires=["numpy"],
     python_requires=">=3"
