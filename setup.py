9854fe5eea69969f579fb013466864e7adf12ff2
Neil P. Quinn <mail@neilpquinn.com>
2018-06-22 16:18:40 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oggini",
+    version="0.1",
+    description="A function for calculating the Gini coefficient of a NumPy array",
+    install_requires=["numpy"],
+    python_requires=">=3"
+)
