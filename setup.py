c540a3f911cf2a36b5a9f6b9df9f654c78b5089f
Neil P. Quinn <mail@neilpquinn.com>
2018-06-22 16:26:03 -0700

    Add missing comma

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author="Olivia Guest",
     url="https://github.com/oliviaguest/gini",
-    license="Creative Commons CC0"
+    license="Creative Commons CC0",
     description="A function for calculating the Gini coefficient of a NumPy array",
     install_requires=["numpy"],
     python_requires=">=3"
